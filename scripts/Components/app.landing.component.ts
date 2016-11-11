46ea1445bda638a30e71d8995c51140a319a9d86
Dominik Raniszewski <dominik.raniszewski@outlook.com>
2016-11-11 17:44:56 +1100

    Service layer

--- scripts/Components/app.landing.component.ts
+++ scripts/Components/app.landing.component.ts
@@ -1,7 +1,10 @@
 /// <reference path='../../typings/tsd.d.ts' />
 
 import { Component, OnInit } from '@angular/core';
+
 import * as D3 from 'd3';
+import * as RestClient from "TFS/WorkItemTracking/RestClient";
+
 
 @Component({
     selector:'landing',
