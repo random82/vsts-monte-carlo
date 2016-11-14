931e3155bdcbb19c6edc4e485e48f877c1decb0b
Dominik Raniszewski <dominik.raniszewski@outlook.com>
2016-11-14 13:07:09 +1100

    Linking up the services

--- scripts/Components/app.landing.component.ts
+++ scripts/Components/app.landing.component.ts
@@ -1,17 +1,22 @@
-/// <reference path='../../typings/tsd.d.ts' />
-
 import { Component, OnInit } from '@angular/core';
+import { WorkItemsService } from '../services/work-items.service';
 
 import * as D3 from 'd3';
-import * as RestClient from "TFS/WorkItemTracking/RestClient";
-
 
 @Component({
     selector:'landing',
-    templateUrl:'./app.landing.component.html'
+    templateUrl:'./app.landing.component.html',
+    providers:[WorkItemsService]
 })
 export class LandingComponent implements OnInit {
+
+    public CompletedWorkItemsCount : number;
+
+    constructor(private workItemsService : WorkItemsService){
+
+    }
+
     ngOnInit() {
-        
+        this.CompletedWorkItemsCount = this.workItemsService.getCompletedWorkItems.length;
     }
 }
