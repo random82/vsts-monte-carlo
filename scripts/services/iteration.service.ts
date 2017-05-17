dcdf64b63fcb07df0bdf24dee184a785bb308a1d
Dominik Raniszewski <dominik.raniszewski@gmail.com>
2017-05-17 17:57:13 +1000

    Client and service boilerplate

---
+++ scripts/services/iteration.service.ts
@@ -0,0 +1,15 @@
+import {
+    Injectable
+} from '@angular/core';
+
+import {
+    IterationClient
+} from './iteration.client.base';
+
+Injectable();
+export class IterationService {
+
+    constructor(private iterationClient : IterationClient){
+        
+    }
+}
---
+++ scripts/services/tfs-iteration.client.ts
@@ -0,0 +1,12 @@
+import {
+    Injectable
+} from '@angular/core';
+
+import {
+    IterationClient
+} from './iteration.client.base';
+
+Injectable();
+export class TfsIterationClient extends IterationClient {
+
+}
